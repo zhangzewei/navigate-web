06d597088a9d6911a8f40786270a3704fe8ea97a
zhangzewei <453646474@qq.com>
2020-07-10 11:08:51 +0800

    feat: layout home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Home from './containers/Home';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      hello world 123
+      <Home />
     </div>
   );
 }
