fb81e87334e96f639ee2bb899b092f9c55d61419
zhangzewei <453646474@qq.com>
2020-08-03 17:46:48 +0800

    feat: 导航栏和主体

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,54 +1,58 @@
 import * as React from 'react';
-import { Layout, Menu } from 'antd';
-import {
-  AppstoreOutlined,
-  BarChartOutlined,
-  CloudOutlined,
-  ShopOutlined,
-  TeamOutlined,
-  UserOutlined,
-  UploadOutlined,
-  VideoCameraOutlined,
-} from '@ant-design/icons';
+import { Layout, Menu, Card, Row, Col } from 'antd';
+import menus from './nav-config.js';
 import './style.scss';
 
 const { Content, Footer, Sider } = Layout;
 
 const Home = () => {
+  const clickMenu = (evnet) => {
+    const titleId = evnet.key;
+    document.getElementById(titleId).scrollIntoView(true);
+  }
+
+  const renderMenu = () => menus.map(menu => (
+    <Menu.Item key={menu.id} >{menu.name}</Menu.Item>
+  ));
+
+  const renderLinks = (links) => links && links.map(link => (
+    <Col span={4}>
+      <a className="link-item" href={link.link} target="blank">
+        <div className="link-title">{link.name}</div>
+        <div className="link-bref">{link.bref}</div>
+      </a>
+    </Col>
+  ));
+
+  const renderMainContainer = () => menus.map(menu => {
+    return (
+      <Card
+        title={
+          <h2
+            id={menu.id}className="title"
+            key={`title-${menu.id}`}
+          ><span># </span>{menu.name}</h2>
+        }
+        style={{ width: '100%', marginBottom: '20px', borderRadius: '8px' }}
+      >
+        <Row gutter={[16, 24]}>
+          {renderLinks(menu.links)}
+        </Row>
+      </Card>
+    )
+  });
+
   return <Layout className="home-page">
     <Sider className="slider">
-      <div className="logo">navigate web</div>
-      <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-        <Menu.Item key="1" icon={<UserOutlined />}>
-          nav 1
-        </Menu.Item>
-        <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-          nav 2
-        </Menu.Item>
-        <Menu.Item key="3" icon={<UploadOutlined />}>
-          nav 3
-        </Menu.Item>
-        <Menu.Item key="4" icon={<BarChartOutlined />}>
-          nav 4
-        </Menu.Item>
-        <Menu.Item key="5" icon={<CloudOutlined />}>
-          nav 5
-        </Menu.Item>
-        <Menu.Item key="6" icon={<AppstoreOutlined />}>
-          nav 6
-        </Menu.Item>
-        <Menu.Item key="7" icon={<TeamOutlined />}>
-          nav 7
-        </Menu.Item>
-        <Menu.Item key="8" icon={<ShopOutlined />}>
-          nav 8
-        </Menu.Item>
+      <div className="logo">学习网站导航</div>
+      <Menu theme="dark" onClick={clickMenu} mode="inline" defaultSelectedKeys={[menus[0].id]}>
+        {renderMenu()}
       </Menu>
     </Sider>
     <Layout className="site-layout">
       <Content className="layout-content">
-        <div className="site-layout-background" style={{ padding: 24, textAlign: 'center' }}>
-            hello world
+        <div className="site-layout-background" id="main-container">
+          {renderMainContainer()}
         </div>
       </Content>
       <Footer className="footer">Navigate Web @2020</Footer>
---
+++ src/containers/Home/nav-config.js
@@ -0,0 +1,19 @@
+export default [
+  {
+    id: 'front-end',
+    name: '前端',
+    links: [
+      {
+        name: 'react',
+        bref: '前端主流框架之一',
+        link: 'https://zh-hans.reactjs.org/'
+      }
+    ],
+  },
+  { id: 'back-end', name: '后端' },
+  { id: 'C', name: 'C/C++/C#' },
+  { id: 'security', name: '网络安全' },
+  { id: 'ui', name: '设计相关' },
+  { id: 'community', name: '论坛社区' },
+  { id: 'update', name: '上报新网址' }
+]
