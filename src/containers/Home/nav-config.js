fdf2afc84c6f15cece786b8b435f667bcd8ed83b
gmhcy <1292647507@qq.com>
2020-08-06 13:21:10 +0800

    nav-config内容

--- src/containers/Home/nav-config.js
+++ src/containers/Home/nav-config.js
@@ -4,16 +4,136 @@ export default [
     name: '前端',
     links: [
       {
+        name:'html',
+        bref:'一种用于创建网页的标准标记语言',
+        link:'https://www.runoob.com/html/html-tutorial.html'
+      },
+      {
+        name:'css',
+        bref:'层叠样式表 (Cascading Style Sheets)',
+        link:'https://www.runoob.com/css/css-intro.html'
+      },
+      {
+        name:'Bootstrap',
+        bref:'一个用于快速开发 Web 应用程序和网站的前端框架',
+        link:'https://www.runoob.com/bootstrap/bootstrap-intro.html'
+      },
+      {
+        name:'jQuery',
+        bref:' JavaScript 库',
+        link:'https://www.runoob.com/jquery/jquery-tutorial.html'
+      },
+      {
         name: 'react',
-        bref: '前端主流框架之一',
+        bref: '前端主流框架之一,用于构建用户界面的 JavaScript 库',
         link: 'https://zh-hans.reactjs.org/'
+      },
+      {
+        name:'vue',
+        bref:'一套用于构建用户界面的渐进式框架',
+        link:'https://cn.vuejs.org/'
+      },
+    ],
+  },
+  { id: 'back-end', 
+    name: '后端',
+    links:[{
+      name:'java',
+      bref:'一门面向对象编程语言,以编写桌面应用程序、Web应用程序、分布式系统和嵌入式系统应用程序等',
+      link:'https://www.runoob.com/java/java-intro.html'
+      },
+      {
+        name:'node.js',
+        bref:'一个事件驱动I/O服务端JavaScript环境，基于Google的V8引擎，V8引擎执行Javascript的速度非常快，性能非常好',
+        link:'https://www.runoob.com/nodejs/nodejs-tutorial.html'
+      },
+      {
+        name:'Python',
+        bref:'一个高层次的结合了解释性、编译性、互动性和面向对象的脚本语言',
+        link:'https://www.runoob.com/python3/python3-intro.html'
+      },
+      {
+        name:'Ruby',
+        bref:'一种纯粹的面向对象编程语言',
+        link:'https://www.runoob.com/ruby/ruby-intro.html'
+      }
+    ], 
+  },
+  { 
+      id: 'C', 
+      name: 'C/C++/C#',
+      links:[{
+        name:'C',
+        bref:'一种通用的、面向过程式的计算机程序设计语言',
+        link:'https://www.runoob.com/cprogramming/c-tutorial.html'
+      },
+      {
+        name:'C++',
+        bref:'一种静态类型的、编译式的、通用的、大小写敏感的、不规则的编程语言，支持过程化编程、面向对象编程和泛型编程',
+        link:'https://www.runoob.com/cplusplus/cpp-intro.html'
+      },
+      {
+        name:'C#',
+        bref:'一个现代的、通用的、面向对象的编程语言',
+        link:'https://www.runoob.com/csharp/csharp-intro.html'
       }
     ],
   },
-  { id: 'back-end', name: '后端' },
-  { id: 'C', name: 'C/C++/C#' },
   { id: 'security', name: '网络安全' },
-  { id: 'ui', name: '设计相关' },
-  { id: 'community', name: '论坛社区' },
+  { 
+      id: 'ui', 
+      name: '设计相关',
+      links:[{
+        name:'iconfont',
+        bref:'国内功能很强大且图标内容很丰富的矢量图标库,提供矢量图标下载、在线存储、格式转换等功能',
+        link:'https://www.iconfont.cn/'
+      },
+      {
+        name:'adobe color',
+        bref:'非常便捷的在线配色网站',
+        link:'https://color.adobe.com/zh/create/color-wheel'
+      },
+      {
+        name:'Unsplash',
+        bref:'高清图片库/壁纸素材网站之一,它不仅纯粹、简单、优雅,同时还带有免费、自由、无版权的逆天属性',
+        link:'https://unsplash.com/'
+      },
+      {
+        name:'Element',
+        bref:'一套为开发者、设计师和产品经理准备的基于 Vue 2.0 的桌面端组件库',
+        link:'https://element.eleme.cn/#/zh-CN'
+      },
+      {
+        name:'Ant Design',
+        bref:'antd是基于Ant Design设计体系的React UI组件库，主要用于研发企业级中后台产品',
+        link:'https://ant.design/index-cn'
+      },
+      {
+        name:'layui',
+        bref:'是一款采用自身模块规范编写的前端 UI 框架，遵循原生 HTML/CSS/JS 的书写与组织形式',
+        link:'https://www.layui.com/doc/'
+      }
+    ],
+   },
+  { 
+      id: 'community', 
+      name: '论坛社区',
+      links:[{
+        name:'掘金',
+        bref:'一个帮助开发者成长的社区,是给开发者用的 Hacker News,给设计师用的 Designer News,和给产品经理用的 Medium',
+        link:'https://juejin.im/'
+      },
+      {
+        name:'牛客网',
+        bref:'一个集笔面试系统、题库、课程教育、社群交流、招聘内推于一体的招聘类网站',
+        link:'https://www.nowcoder.com/'
+      },
+      {
+        name:'CSDN',
+        bref:'全球知名中文IT技术交流平台',
+        link:'https://www.csdn.net/'
+      },
+  ]
+   },
   { id: 'update', name: '上报新网址' }
 ]
